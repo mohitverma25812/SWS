c5ae0acffe2c4a6216827309376b8c4cc370717e
mohitverma25812 <150012820+mohitverma25812@users.noreply.github.com>
2026-04-25 17:58:13 +0530

    add to booking and add to service type

--- backend/models/Booking.js
+++ backend/models/Booking.js
@@ -17,6 +17,10 @@ const BookingSchema = new mongoose.Schema({
         type: String, 
         required: true 
     },
+    subService: {
+    type: String,
+    default: ""
+},
     location: { 
         type: String, 
         default: "Live Location" 
